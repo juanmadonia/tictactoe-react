846742f4f01c216de06cf78c4cbc410924fd740c
Juan Madonia <madval@gmail.com>
2023-06-07 19:50:22 -0700

    Taking turns on empty squares

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import { useState } from "react";
 
 function Square({ value, onSquareClick }) {
-//function Square() {
-  //const [value, setValue] = useState(null);
-
-  //function handleClick() {
-    //setValue('X');
-  //}
-
-  //return <button className="square">{ value }</button>;
   return (
     <button 
       className="square" 
@@ -18,11 +10,20 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if(squares[i]) return;
+
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    
+    if(xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setXIsNext(!xIsNext);
     setSquares(nextSquares);
   }
 
